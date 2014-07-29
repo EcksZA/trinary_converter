31f0fbec1b9ecfa6035832b86c169bedae873899
Dustin Lewis & Xolani Mfuko <student@epicodus.com>
2014-07-29 15:15:06 -0700

    refactors function to eliminate branching

--- lib/trinary.rb
+++ lib/trinary.rb
@@ -1,7 +1,9 @@
 def trinary_convert(input)
   number = input.to_i
+  boxnumber = 2
   position = 0
   trinary_store = []
+  empty_check = 0
 
   while 3**position <= number do
     position += 1
@@ -10,19 +12,16 @@ def trinary_convert(input)
   position -= 1
 
   position.downto(0) do |base_power|
-
-    if(number - 2*(3**base_power) >= 0)
-      trinary_store << "2"
-      number -= 2*(3**base_power)
-    elsif (number - 3**base_power >= 0)
-      trinary_store << "1"
-      number -= 3**base_power
-    elsif
-      trinary_store << "0"
+    boxnumber.downto(0) do |count|
+      if(number - count*(3**base_power) >= 0 && empty_check == 0)
+        trinary_store << count
+        empty_check = count
+        number -= count*(3**base_power)
+      end
     end
+    empty_check = 0
   end
-
   trinary_store.join("").to_i
 end
 
-#puts trinary_convert("200")
+puts trinary_convert("200")
